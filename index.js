18400cbb16da9862d5bf9300b1840dd0ffd7a09e
olenleo <50656714+olenleo@users.noreply.github.com>
2019-07-25 17:36:37 +0300

    Kurssitiedot 1.5

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const Header = (props) => {
             <h1> {props.course} </h1>
         </div>
     )
-
 }
 
 const Content = (props) => {
@@ -20,41 +19,40 @@ const Content = (props) => {
             <p>{props.parts[2].name} {props.parts[2].exercises} </p>
         </div>
     )
-
 }
 const Total = (props) => {
 
     return (
         <div>
-            {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+            <p> {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
         </div>
     )
-
 }
 const App = () => {
-    // const-määrittelyt
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
 
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
     return (
         <div>
-            <Header course={course} />
-            <Content parts={parts} />
-            <Total parts={parts} />
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </div>
     )
 }
