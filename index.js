5a2a2024571df8d257350c318af6f69057b32462
olenleo <50656714+olenleo@users.noreply.github.com>
2019-08-19 21:51:28 +0300

    Osa 1.14 - Anekdootit, step3

--- index.js
+++ index.js
@@ -1,62 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-const Header = (props) => {
 
-    return (
-        <div>
-            <h1> {props.course} </h1>
-        </div>
-    )
-}
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+        {text}
+    </button>
+)
 
-const Content = (props) => {
 
-    return (
-        <div>
-            <p>{props.parts[0].name} {props.parts[0].exercises} </p>
-            <p>{props.parts[1].name} {props.parts[1].exercises} </p>
-            <p>{props.parts[2].name} {props.parts[2].exercises} </p>
-        </div>
-    )
-}
-const Total = (props) => {
 
-    return (
-        <div>
-            <p> {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-        </div>
-    )
-}
-const App = () => {
+const App = (props) => {
+    const [selected, setSelected] = useState(0)
+    const [voted, setVoted] = useState(new Array(6).fill(0))
 
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
+    const generateNewAnecdote = newValue => {
+        setSelected(newValue)
+        generateNewRandomNumber()
+    }
+    const generateNewRandomNumber = () => {
+        return (
+            randomNumberGenerator()
+        )
+    }
+
+    const getHighestScore = () => {
+        let indexOfHighest = 0
+        for (var i = 0; i < voted.length - 1; i++) {
+            if (voted[i] > voted[indexOfHighest]) {
+                indexOfHighest = i
             }
-        ]
+        }
+        return ([indexOfHighest])
+
+    }
+
+    const vote = () => {
+        const voteCopy = [...voted]
+        voteCopy[selected] += 1
+        return (voteCopy)
+    }
+
+    const randomNumberGenerator = () => {
+        const rndNumber = Math.ceil(Math.random() * anecdotes.length - 1)
+        return rndNumber
     }
 
     return (
         <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
+            <h1>Anecdote of the day</h1>
+            {props.anecdotes[selected]}
+            <p> has {voted[selected]} votes</p>
+            <p>
+                <Button handleClick={() => setVoted(vote())}
+                    text='Vote' />
+
+
+                <Button handleClick={() => generateNewAnecdote(randomNumberGenerator(anecdotes.length))}
+                    text='next anecdote' />
+            </p>
+
+
+            <h1>Anecdote with the most votes</h1>
+            <p>{anecdotes[getHighestScore()]}</p>
+            <p>Entire array: {voted}</p>
+
+
         </div>
     )
 }
 
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <App anecdotes={anecdotes} />,
+    document.getElementById('root')
+)
