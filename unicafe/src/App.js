aa9d1423b3a6a01215fddb08ed00a68eabe37a79
Leo Niemi <niemi.leo@gmail.com>
2021-08-17 17:04:37 +0300

    Unicafe step5 1.10

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,23 +1,47 @@
 import React, { useState } from 'react'
 
+
+const Statistics = ({goodVal, neutralVal, badVal}) => {
+  return (
+  <div>
+  <h1>Statistics</h1>
+  <StatisticsLine text = "good" value = {goodVal} />
+  <StatisticsLine text = "neutral" value = {neutralVal} />
+  <StatisticsLine text = "bad" value = {badVal} />
+  <StatisticsLine text = "total" value = {goodVal + neutralVal + badVal} />
+  <StatisticsLine text = "average" value = {goodVal / badVal} />
+  
+  
+  </div>
+  )
+}
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const StatisticsLine = ({ text, value }) => {
+  return (
+  <p>{text} : {value}</p>
+  )
+}
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
 
+
+ 
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-      
-      <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-
+      <Button handleClick ={() =>setGood(good + 1)} text = "good"/>
+      <Button handleClick ={() =>setNeutral(neutral + 1)} text = "neutral"/>
+      <Button handleClick ={() =>setBad(bad + 1)} text = "bad"/>
+      <Statistics goodVal = {good} neutralVal = {neutral} badVal = {bad} />
     </div>
     
   )
