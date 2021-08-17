cf141383d95e927ba80c677d9e1f40bcc058060d
Leo Niemi <niemi.leo@gmail.com>
2021-08-17 16:21:12 +0300

    Unicafe 1.6 step 1

---
+++ unicafe/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+
+    </div>
+    
+  )
+}
+
+export default App
