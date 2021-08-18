1034fade65d1948619dc1f3da5468133f18e5d5d
Leo Niemi <niemi.leo@gmail.com>
2021-08-18 10:43:03 +0300

    Lisätty unohtunut unicafe:n ehdollinen renderöinti

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -2,7 +2,13 @@ import React, { useState } from 'react'
 
 
 const Statistics = ({goodVal, neutralVal, badVal}) => {
-  return (
+  if (goodVal === 0 && neutralVal === 0 && badVal === 0) {
+    return (
+      <div>
+        <h2>No feedback given</h2>
+      </div>
+    )
+  } else return (
   <div>
   <h1>Statistics</h1>
   <table>
@@ -31,7 +37,7 @@ const StatisticsLine = ({ text, value }) => {
   )
 }
 const App = () => {
-  // tallenna napit omaan tilaansa
+  
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -39,6 +45,7 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
+      
       <Button handleClick ={() =>setGood(good + 1)} text = "good"/>
       <Button handleClick ={() =>setNeutral(neutral + 1)} text = "neutral"/>
       <Button handleClick ={() =>setBad(bad + 1)} text = "bad"/>
